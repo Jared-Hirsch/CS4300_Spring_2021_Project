ffaafe2d85b6f7baf403e86a791e777befe48a23
Jared Hirsch <jsh328@cornell.edu>
2021-04-05 14:49:44 -0400

    Add in names and project

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,9 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Twitter Account Recommendations"
+net_ids = ["Jared Hirsch: jsh328", "Ronnie Dumesh: rad338", "Ravina Patel: rpp62", "Chris Chen: cmc447"]
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -14,7 +14,4 @@ def search():
 	else:
 		output_message = "Your search: " + query
 		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
+	return render_template('search.html', name=project_name, netids=net_ids, output_message=output_message, data=data)
