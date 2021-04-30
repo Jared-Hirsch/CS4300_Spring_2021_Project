5af150a543e1b5d559f7256c4b97c4b8b7440d14
Jared Hirsch <jsh328@cornell.edu>
2021-04-30 18:03:59 -0400

    Proper import of utils again

--- app/irsystem/SimSongs.py
+++ app/irsystem/SimSongs.py
@@ -16,8 +16,7 @@ from sp_client import Spotify_Client
 import string
 import os
 # from app.irsystem.utils import strip_name, match
-# from app.irsystem.utils import *
-from utils import *
+from app.irsystem.utils import *
 
 
 punct = set(string.punctuation)
